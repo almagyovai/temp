a52e852f6bbb3429f95284e74d8f963dd0d46934
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-06-09 22:17:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,92 @@
-const data = [
-    "testing", // string
-    false, // boolean
-    30, // number
-    { model: "3", color: "red" ), // object
-    [1, 2, 3, 4, 5], // array
-    ]; // length = 5, but last index = 4
+// Addition - two numbers
 
-
-const multiplier = data[4];
-
-for (let i; i < multiplier.length; i++) {
-    console.log("i:", i);
+function addNumbers(x, y) {
+    return x + y;
 }
+
+
+let x = 5;
+let y = 10;
+
+
+sum = 15;
+
+
+console.log("The sum of" + " " + x + " " + "and" + " " + y + " " + "is"  + " " + sum);
+
+
+
+// Addition - three numbers
+
+function addNumbers(a, b, c) {
+    return a + b + c;
+}
+
+
+let a = 3;
+let b = 5;
+let c = 9;
+
+
+sum = addNumbers(a, b, c);
+
+
+console.log("The sum of" + " " + a + "," + " " + b + " " + "and" + " " + c + " " + "is" + " " + sum);
+
+
+
+// Comparison - two numbers
+
+function compareNumbers(e, d) {
+    if (e > d) {
+      console.log("e is bigger than d.");
+    } else {
+      console.log("e is bigger or equal to d.");
+    }
+  };
+
+
+compareNumbers(6, 2);
+
+
+
+// Age Verification
+
+function displayMessage() {
+    let age = prompt("What is your age?");
+    
+    age = Number(age);
+
+      if (age >= 18) {
+        message = "You are 18 or older.";
+      } else {
+        message = "You are younger than 18.";
+      }
+  
+      console.log(message);
+    };
+
+  displayMessage();
+
+
+
+  // Conditions 
+
+function displayMessage() {
+    let condition = prompt("Is it sunny or is it rainy?")
+
+    let message = condition(rainy, sunny);
+
+    if (rainy && sunny) {
+        message = "Both conditions are true.";
+    } else if (rainy) {
+        message = "At least one condition is false.";
+    }
+    else if (sunny) {
+        message = "At least one condition is false.";
+    };
+
+    console.log(message);
+}
+
+displayMessage();
